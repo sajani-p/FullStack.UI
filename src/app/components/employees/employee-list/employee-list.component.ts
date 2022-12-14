3ee7ce395e649fc6e1f603029ef03f48056e8496
sajee <sajipathegama@gmail.com>
2022-12-14 09:38:32 +0530

    create employee list

---
+++ src/app/components/employees/employee-list/employee-list.component.ts
@@ -0,0 +1,43 @@
+//employee la hadanne and backend eken push krnne me file eken
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit{
+
+  employees: Employee[] = [
+    {
+      id: 'emp001',
+      name: 'Sajani Pathegama',
+      email: 'sajanipathegama@gmail.com',
+      phone: 715670110,
+      salary: 12250,
+      department: 'IT',
+    },
+    {
+      id: 'emp002',
+      name: 'Kishan Randika',
+      email: 'kishan@gmail.com',
+      phone: 766552013,
+      salary: 50000,
+      department: 'Mechanical',
+    },
+    {
+      id: 'emp003',
+      name: 'Lumini Pathegama',
+      email: 'luminipathegama@gmail.com',
+      phone: 702088268,
+      salary: 37000,
+      department: 'Manager',
+    }
+  ]
+  constructor(){}
+
+  ngOnInit():void{
+    //this.employees.push();
+  }
+}
